e1224f28b54e6b03eebe42bf6b853ea68899bd11
janwieners <jan@jan-wieners.de>
2019-09-16 23:31:51 +0200

    Add some examples from the book.

---
+++ Programming-In-Haskell-2nd-Edition/ch-04_playground.hs
@@ -0,0 +1,28 @@
+module Chapter_04_Playground where
+
+-- Conditional expressions
+abs' :: Int -> Int
+abs' n = if n >= 0 then n else -n
+
+-- Tuple patterns
+fst' :: (a,b) -> a
+fst' (x,_) = x
+
+snd' :: (a,b) -> b
+snd' (_,y) = y
+
+-- List patterns
+test :: [Char] -> Bool
+test ['a',_,_]  = True
+test _          = False
+
+-- Cons operator
+test' :: [Char] -> Bool
+test' ('a':_)   = True
+test' _         = False
+-- test' "abcd"
+-- test' "bcd"
+
+-- Lambda expressions
+add' :: Int -> (Int -> Int)
+add' = \x -> (\y -> x + y)
