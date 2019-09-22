0b52f76512e72c7c73ac53617149e062fbaad2ec
Jan G. Wieners <jan@jan-wieners.de>
2019-09-22 13:25:52 +0200

    Add solution for 04-03.

---
+++ Programming-In-Haskell-2nd-Edition/ch-04-03_safetail.hs
@@ -0,0 +1,19 @@
+module Chapter_04_03 where
+
+    -- conditional expression
+    safetail :: [a] -> [a]
+    safetail xs =
+                if null xs
+                    then []
+                else tail xs
+
+    -- guarded equations
+    safetail' :: [a] -> [a]
+    safetail' xs
+        | null xs = []
+        | otherwise = tail xs
+
+    -- pattern matching
+    safetail'' :: [a] -> [a]
+    safetail'' [] = []
+    safetail'' (_:xs) = xs
