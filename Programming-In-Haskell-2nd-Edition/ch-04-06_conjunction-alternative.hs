2b503d6b78655fad692d361beb3cba0f1eb6e492
Jan G. Wieners <jan@jan-wieners.de>
2019-09-23 20:51:04 +0200

    Add solution for 04-06.

---
+++ Programming-In-Haskell-2nd-Edition/ch-04-06_conjunction-alternative.hs
@@ -0,0 +1,16 @@
+module Chapter_04_05 where
+
+    {-
+        True && b = b
+        False && _ = False
+
+        True && True = True
+        True && False = False
+
+        False && True = False
+        False && False = False
+    -}
+
+    (&&&) :: Bool -> Bool -> Bool
+    -- a &&& b = if a then b else if False then False else False
+    a &&& b = if a then b else False
